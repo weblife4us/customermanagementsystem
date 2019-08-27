bad25e83de75020f3f761b47774aa70c9360dec3
pavlokorsun <44420941+pavlokorsun@users.noreply.github.com>
2019-08-27 13:18:49 -0400

    test commit

--- src/main/java/com/project/basic/controller/HelloWorldController.java
+++ src/main/java/com/project/basic/controller/HelloWorldController.java
@@ -5,14 +5,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 public class HelloWorldController {
 
-
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
+
     public String getHelloWorldMessage() {
-
-
         return "<div style=\"text-align:center;\">" + "<h1>Hello world</h1>"
                 + "<p> This is my first springboot page </p>"
                 + "</div>";
